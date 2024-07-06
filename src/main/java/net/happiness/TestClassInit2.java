063a6210fa06a48c0b8eae0bc244940752d5fd0e
Leonid Pipchenkov <t70a80@gmail.com>
2024-07-06 16:06:15 +0300

    Adding a class initialization examples

---
+++ src/main/java/net/happiness/TestClassInit2.java
@@ -0,0 +1,27 @@
+package net.happiness;
+
+public class TestClassInit2 {
+
+    public static void main(String[] args) {
+        try {
+            new InitTest2();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        try {
+            new InitTest2();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
+class InitTest2 {
+    static {
+        System.out.println("In initializer");
+        if (true) {
+            throw new RuntimeException("Oops");
+        }
+    }
+}
---
+++ src/main/java/net/happiness/TestClassInit3.java
@@ -0,0 +1,23 @@
+package net.happiness;
+
+public class TestClassInit3 {
+
+    static final String a = "String a";
+    static final String b = "String b".trim();
+    static final String c = init();
+    static final String d = "String " + "d";
+    static final String e = "String e".trim();
+
+    private static String init() {
+        System.out.println("a = " + a); // a = String a
+        System.out.println("b = " + b); // b = String b
+        System.out.println("c = " + c); // c = null
+        System.out.println("d = " + d); // d = String d
+        System.out.println("e = " + e); // e = null
+        return "String c";
+    }
+
+    public static void main(String[] args) {
+    }
+
+}
